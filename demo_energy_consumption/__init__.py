9c97b957765ffc3fc532620bf5970a7a2fc6a1e9
dm250067 <denis.molin@teradata.com>
2021-09-02 09:41:53 +0200

    first function + test added

--- demo_energy_consumption/__init__.py
+++ demo_energy_consumption/__init__.py
@@ -1,5 +1,14 @@
-"""Top-level package for demo energy consumption."""
-
+"""
+Demo Energy Consumption for Python
+============================
+The demo energy consumption package is a basic demonstration of a Python
+package leveraging Vantage.
+It is a package example that provides a Python interface to manipulate data
+stored in Vantage. It pushes downs to Vantage the workload so that the user
+can benefit from the MPP engine and the data preparation capabilities provi-
+ded by Teradata Vantage in their client running Python codes.
+"""
+__version__ = '0.1.0'
 __author__ = """Denis Molin"""
 __email__ = 'denis.molin@teradata.com'
 __version__ = '0.1.0'
--- tests/test_demo_energy_consumption.py
+++ tests/test_demo_energy_consumption.py
@@ -35,3 +35,11 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_get_my_data():
+    """Test  get_my_data"""
+
+    expected_result = "\n    select \n       \n         $TD_TIMECODE_RANGE as TD_TIMECODE_RANGE\n        ,CAST(BEGIN($TD_TIMECODE_RANGE) AS VARCHAR(32)) as START_PERIOD\n        ,CAST(END($TD_TIMECODE_RANGE) AS VARCHAR(32)) as END_PERIOD\n        \n    -- dimensions\n      ,region,profil,plage_de_puissance_souscrite\n    -- kpis\n      ,sum(total_energie_soutiree_wh) as sum_total_energie_soutiree_wh,avg(courbe_moyenne_num1_num2_Wh) as avg_courbe_moyenne_num1_num2_Wh\n      from  ADLSLSEMEA_ENEDIS.Enedis_conso_hot\n      \n        group by TIME(\n        hours(6)\n        AND region,profil,plage_de_puissance_souscrite)\n        using timecode(horodate)\n      where horodate between DATE '2020-12-01' and DATE '2020-12-31'\n    "
+    assert demo_energy_consumption.get_my_data(
+        table_name='Enedis_conso_hot') == expected_result
