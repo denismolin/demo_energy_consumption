b4fdafe50badc97d4b2a82b49b77c56d8b0f216f
dm250067 <denis.molin@teradata.com>
2023-01-02 15:00:20 +0100

    latest update

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/denismolin/demo_energy_consumption',
-    version='0.2.2',
+    version='0.2.3',
     zip_safe=False,
 )
